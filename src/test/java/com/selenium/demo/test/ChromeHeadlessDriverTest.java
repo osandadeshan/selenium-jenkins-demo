277e880c55028515a7d16497f8684dc5e9f37971
Osanda Deshan <osanda.deshan@gmail.com>
2020-07-13 08:33:52 +0530

    TestNg configs

--- src/test/java/com/selenium/demo/test/ChromeHeadlessDriverTest.java
+++ src/test/java/com/selenium/demo/test/ChromeHeadlessDriverTest.java
@@ -1,14 +1,14 @@
 package com.selenium.demo.test;
 
 import com.maxsoft.webdrivermanager.driver.DriverFactory;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 /**
  * Project Name    : maxsoft-webdriver-manager-demo
@@ -26,7 +26,7 @@ public class ChromeHeadlessDriverTest {
     public static WebDriver driver;
 
     // Initialize a headless driver instance of chrome browser
-    @Before
+    @BeforeTest
     public void initializeDriver() {
         driver = DriverFactory.getDriver(DriverFactory.HEADLESS_CHROME);
         driver.manage().window().maximize();
@@ -36,17 +36,17 @@ public class ChromeHeadlessDriverTest {
     public void checkBrowser() {
         Capabilities cap = ((RemoteWebDriver) driver).getCapabilities();
         String runningBrowserName = cap.getBrowserName().toLowerCase();
-        Assert.assertEquals(BrowserType.CHROME, runningBrowserName);
+        Assert.assertEquals(runningBrowserName, BrowserType.CHROME);
     }
 
     @Test
     public void checkPageTitle() {
         driver.get("http://automationpractice.com/");
-        Assert.assertEquals("My Store", driver.getTitle());
+        Assert.assertEquals(driver.getTitle(), "My Store");
     }
 
     // Close the chrome headless driver instance
-    @After
+    @AfterTest
     public void closeDriver() {
         driver.quit();
     }
